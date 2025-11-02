d11803d86024ace0f6b07933cc67d207ec288099
Dharmendra Patel <132144447+Dharmendra4455@users.noreply.github.com>
2025-11-02 09:46:02 +0530

    Add appointment routes for data and status updates

--- HMSB/Routers/Doctor.js
+++ HMSB/Routers/Doctor.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import { doctors } from '../Controller/Doctor.js'
 import { doclogin } from '../Controller/Doctor.js'
+import { appointmentdatas } from '../Controller/Doctor.js'
+import { appointmentupdate } from '../Controller/appointments.js'
 export const Docroute=express.Router()
 Docroute.post('/signup',doctors)
 export const docloginroute=express.Router()
 docloginroute.post('/login',doclogin)
+export const appointmentdata=express.Router()
+appointmentdata.get('/appointmentdata',appointmentdatas)
+export const appointmentstatus =express.Router()
+ appointmentstatus.post('/appointmentstatus',appointmentupdate)
